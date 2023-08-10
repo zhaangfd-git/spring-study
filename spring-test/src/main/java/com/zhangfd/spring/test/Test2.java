8cc856d29c40d91931c6ca91c9049e823ec59343
zhaangfd-git <785439353@qq.com>
2023-08-10 09:55:25 +0800

    day10

--- spring-test/src/main/java/com/zhangfd/spring/test/Test2.java
+++ spring-test/src/main/java/com/zhangfd/spring/test/Test2.java
@@ -1,7 +1,7 @@
 package com.zhangfd.spring.test;
 
-import com.zhangfd.spring.BeanWrapper;
-import com.zhangfd.spring.BeanWrapperImpl;
+import com.zhangfd.spring.*;
+import com.zhangfd.spring.util.StringUtils;
 
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
@@ -10,6 +10,7 @@ import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Test2 {
 
@@ -22,18 +23,43 @@ public class Test2 {
         BeanWrapper beanWrapper = new BeanWrapperImpl(new BeanInfoTest());
         //获取实例的所有属性
         PropertyDescriptor[] propertyDescriptors02 = beanWrapper.getPropertyDescriptors();
-        Class<?> wrappedClass = beanWrapper.getWrappedClass();
+        //Class<?> wrappedClass = beanWrapper.getWrappedClass();
+
+        //获取包装对象具体的属性
+        for (PropertyDescriptor propertyDescriptor:propertyDescriptors02) {
+            if("class".equalsIgnoreCase(propertyDescriptor.getName()) || StringUtils.isEmpty(propertyDescriptor.getName())){
+                continue;
+            }
+            System.out.println(propertyDescriptor.getName());
+            System.out.println(propertyDescriptor.getPropertyType());
+            System.out.println(propertyDescriptor.getReadMethod());
+            System.out.println(propertyDescriptor.getWriteMethod());
+        }
 
         HashMap<String, List<Integer>> param = new HashMap<>();
         List<Integer>  tt = new ArrayList<>();
         tt.add(1);tt.add(2);param.put("test",tt);
+        //给包装对象的属性赋值
+      //  beanWrapper.setPropertyValue("param",param);
 
-        beanWrapper.setPropertyValue("param",param);
-        beanWrapper.setPropertyValue("name","zhangfd");
+        beanWrapper.setPropertyValue(new PropertyValue("age","18"));
 
+        Map map = new HashMap<>();
+        map.put("name", "zhangmm");
+        beanWrapper.setPropertyValues(map);
+
+        MutablePropertyValues ps = new MutablePropertyValues();
+        ps.addPropertyValue("param",param);
+        beanWrapper.setPropertyValues(ps);
 
         Object wrappedInstance = beanWrapper.getWrappedInstance();
-        Integer integer = beanWrapper.convertIfNecessary("8", Integer.class);
+
+        //做类型转换:spring内置了大量了常用的属性编辑器，可自动完成类型转换工作
+        BeanWrapper beanWrapper01 = new BeanWrapperImpl();
+        Integer integer = beanWrapper01.convertIfNecessary("8", Integer.class);
+
+        //还可以定义自己的属性编辑器
+        //beanWrapper01.registerCustomEditor();
     }
 
 
@@ -47,6 +73,8 @@ public class Test2 {
 
         private String  name;
 
+
+        private Integer  age;
 
         public HashMap<String, List<Integer>> getParam() {
             return param;
@@ -62,6 +90,14 @@ public class Test2 {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
         }
     }
 }
