42b801beee1e7e11468cea46c7144bfefbd7c236
zhaangfd-git <785439353@qq.com>
2023-12-14 17:17:58 +0800

    day17

--- spring-context/src/test/java/com/zhangfd/spring/context/test/ResourceEditorRegistrarTest.java
+++ spring-context/src/test/java/com/zhangfd/spring/context/test/ResourceEditorRegistrarTest.java
@@ -1,8 +1,10 @@
 package com.zhangfd.spring.context.test;
 
+import com.zhangfd.spring.beans.PropertyEditorRegistrar;
 import com.zhangfd.spring.beans.support.ResourceEditorRegistrar;
 import com.zhangfd.spring.core.env.StandardEnvironment;
 import com.zhangfd.spring.core.io.support.PathMatchingResourcePatternResolver;
+import com.zhangfd.spring.factory.support.PropertyEditorRegistrySupport;
 
 /**
  * @author: zhangfd
@@ -13,9 +15,18 @@ import com.zhangfd.spring.core.io.support.PathMatchingResourcePatternResolver;
 public class ResourceEditorRegistrarTest {
 
     public static void main(String[] args) {
+        //主要是添加类型转换器，主要是把字符串传唤为java对象，
         ResourceEditorRegistrar registrar = new ResourceEditorRegistrar(new PathMatchingResourcePatternResolver(Thread.currentThread().getContextClassLoader()),new StandardEnvironment());
 
-        //registrar.registerCustomEditors();
+        registrar.registerCustomEditors(new PropertyEditorRegistrySupport());
+
+        //PropertyEditorRegistrar的实现类是ResourceEditorRegistrar
+        //目的是 可以自定义PropertyEditorRegistry实现类，
+        // spring已经给了非常好用的默认实现类PropertyEditorRegistrySupport
+
+
+        //PropertyEditorRegistry接口的目的是包装PropertyEditor接口，它的默认实现类PropertyEditorSupport
+        //就是属性类型转换器
     }
 
 }
